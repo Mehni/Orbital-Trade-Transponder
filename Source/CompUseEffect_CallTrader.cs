70d55f8e135a26de908a001e175954b0b888e06c
Mehni <Mehni@users.noreply.github.com>
2018-11-05 09:45:31 +0100

    Update to RW 1.0

--- Source/CompUseEffect_CallTrader.cs
+++ Source/CompUseEffect_CallTrader.cs
@@ -24,11 +24,10 @@ namespace TraderWhereAreYou
                 TradeShip tradeShip = new TradeShip(def);
                 if (map.listerBuildings.allBuildingsColonist.Any((Building b) => b.def.IsCommsConsole && b.GetComp<CompPowerTrader>().PowerOn))
                 {
-                    Find.LetterStack.ReceiveLetter(tradeShip.def.LabelCap, "TraderArrival".Translate(new object[]
-                    {
+                    Find.LetterStack.ReceiveLetter(tradeShip.def.LabelCap, "TraderArrival".Translate(
                         tradeShip.name,
                         tradeShip.def.label
-                    }), LetterDefOf.PositiveEvent, null);
+                    ), LetterDefOf.PositiveEvent, null);
                 }
                 map.passingShipManager.AddShip(tradeShip);
                 tradeShip.GenerateThings();
